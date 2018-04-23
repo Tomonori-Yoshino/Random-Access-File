4b03a12ac61db9b4bd528deb4b17c3d6c1c38978
Tomonori Yoshino <35744287+Tomonori-Yoshino@users.noreply.github.com>
2018-04-23 16:21:02 -0500

    Update CampsiteDB.h

--- CampsiteDB.h
+++ CampsiteDB.h
@@ -6,27 +6,27 @@
 
 class CampsiteDB {
 public:
+    //constructor
     CampsiteDB( std::string filename );
 
+    //member methods
     int get_record_count( );
     int get_current_index( bool write = false );
 
     Campsite get_next_sequential( );
     Campsite get_at_index( int index );
+    Campsite get_random();
 
     void write_next_sequential( const Campsite& site );
     void write_at_index( int index, const Campsite& site );
-
     void print_record( int index, std::ostream& strm = std::cout );
     void list_records( std::ostream& strm = std::cout );
-
     void move_to_index( int index );
     void swap_records( int index_1, int index_2 );
+
     std::vector<Campsite> get_range( int first_index, int last_index );
-    Campsite get_random() { return get_at_index( rand()%100 ); }
 
-
-    bool bounds_check( int index, bool write = false );
+    bool bounds_check( int index, bool write = false );  //helper method
 
     // This object is non-copyable
     CampsiteDB(const CampsiteDB&)            = delete;
